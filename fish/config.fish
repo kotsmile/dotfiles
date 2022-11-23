f3e082e3307e17905ef94e787d6dc568b2d5ad3f
Gregory Rogachev <gregory.rogachev@gotbit.io>
2022-11-23 11:57:16 +0300

    update dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,15 @@
 
 if status is-interactive
 end
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+
+# pnpm
+set -gx PNPM_HOME "/Users/kotsmile/Library/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
+
+bind -M insert \cf accept-autosuggestion

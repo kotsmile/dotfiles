4d678f8478cfe861df3c5bc7d0ad6703b6d5caa3
Henry G <henry@chanity.io>
2025-08-04 04:08:33 +0300

    upd

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -5,19 +5,19 @@ vim.g.neovide_text_gamma = 0.0
 vim.opt.colorcolumn = "100"
 
 return {
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   opts = { flavour = "macchiato" },
-  --   priority = 1000
-  -- },
   {
     "catppuccin/nvim",
-    enabled = true,
-    lazy = false,
     name = "catppuccin",
     opts = { flavour = "macchiato" },
+    priority = 1000,
   },
+  -- {
+  --   "catppuccin/nvim",
+  --   enabled = true,
+  --   lazy = false,
+  --   name = "catppuccin",
+  --   opts = { flavour = "macchiato" },
+  -- },
   -- { "rose-pine/neovim", name = "rose-pine", init = function() vim.cmd "colorscheme rose-pine-moon" end },
   -- { "NLKNguyen/papercolor-theme", name = "papercolor" },
   -- {
@@ -58,7 +58,12 @@ return {
   },
   { "indent-blankline.nvim", enabled = false },
   { "lewis6991/gitsigns.nvim", enabled = true },
-  { "Mofiqul/dracula.nvim" },
+  -- {
+  --   "Mofiqul/dracula.nvim",
+  --   name = "dracula",
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
   {
     "ellisonleao/gruvbox.nvim",
   },
@@ -81,96 +86,218 @@ return {
     lazy = false,
     opts = {},
   },
-  -- {
-  --   'renerocksai/telekasten.nvim',
-  --   dependencies = { 'nvim-telescope/telescope.nvim' },
-  --   enabled = true,
-  --   lazy = false
-  -- },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+    opts = {
+      ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+      provider = "claude", -- Recommend using Claude
+      -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
+      -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
+      -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
+      auto_suggestions_provider = "claude",
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-3-5-sonnet-20241022",
+        temperature = 0,
+        max_tokens = 4096,
+        api_key_name = "ANTHROPIC_API_KEY",
+      },
+      ---Specify the special dual_boost mode
+      ---1. enabled: Whether to enable dual_boost mode. Default to false.
+      ---2. first_provider: The first provider to generate response. Default to "openai".
+      ---3. second_provider: The second provider to generate response. Default to "claude".
+      ---4. prompt: The prompt to generate response based on the two reference outputs.
+      ---5. timeout: Timeout in milliseconds. Default to 60000.
+      ---How it works:
+      --- When dual_boost is enabled, avante will generate two responses from the first_provider and second_provider respectively. Then use the response from the first_provider as provider1_output and the response from the second_provider as provider2_output. Finally, avante will generate a response based on the prompt and the two reference outputs, with the default Provider as normal.
+      ---Note: This is an experimental feature and may not work as expected.
+      dual_boost = {
+        enabled = false,
+        first_provider = "openai",
+        second_provider = "claude",
+        prompt = "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+        timeout = 60000, -- Timeout in milliseconds
+      },
+      behaviour = {
+        auto_suggestions = false, -- Experimental stage
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+        minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+      },
+      mappings = {
+        diff = {
+          ours = "co",
+          theirs = "ct",
+          all_theirs = "ca",
+          both = "cb",
+          cursor = "cc",
+          next = "]x",
+          prev = "[x",
+        },
+        suggestion = {
+          accept = "<M-l>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+        jump = {
+          next = "]]",
+          prev = "[[",
+        },
+        submit = {
+          normal = "<CR>",
+          insert = "<C-s>",
+        },
+        sidebar = {
+          apply_all = "A",
+          apply_cursor = "a",
+          switch_windows = "<Tab>",
+          reverse_switch_windows = "<S-Tab>",
+        },
+      },
+      hints = { enabled = true },
+      windows = {
+        ---@type "right" | "left" | "top" | "bottom"
+        position = "right", -- the position of the sidebar
+        wrap = true, -- similar to vim.o.wrap
+        width = 30, -- default % based on available width
+        sidebar_header = {
+          enabled = true, -- true, false to enable/disable the header
+          align = "center", -- left, center, right for title
+          rounded = true,
+        },
+        input = {
+          prefix = "> ",
+          height = 8, -- Height of the input window in vertical layout
+        },
+        edit = {
+          border = "rounded",
+          start_insert = true, -- Start insert mode when opening the edit window
+        },
+        ask = {
+          floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+          start_insert = true, -- Start insert mode when opening the ask window
+          border = "rounded",
+          ---@type "ours" | "theirs"
+          focus_on_apply = "ours", -- which diff to focus after applying
+        },
+      },
+      highlights = {
+        ---@type AvanteConflictHighlights
+        diff = {
+          current = "DiffText",
+          incoming = "DiffAdd",
+        },
+      },
+      --- @class AvanteConflictUserConfig
+      diff = {
+        autojump = true,
+        ---@type string | fun(): any
+        list_opener = "copen",
+        --- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
+        --- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
+        --- Disable by setting to -1.
+        override_timeoutlen = 500,
+      },
+      suggestion = {
+        debounce = 600,
+        throttle = 600,
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+  {
+    "chrisbra/csv.vim",
+    ft = { "csv" },
+  },
+  { import = "astrocommunity.pack.toml" },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = {
+      -- handlers = { rust_analyzer = false }, -- disable setup of `rust_analyzer`
+      ---@diagnostic disable: missing-fields
+      config = {
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              files = {
+                excludeDirs = {
+                  ".direnv",
+                  ".git",
+                  "target",
+                },
+              },
+              check = {
+                command = "clippy",
+                extraArgs = {
+                  "--no-deps",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "swaits/zellij-nav.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    keys = {
+      { "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>", { silent = true, desc = "navigate left or tab" } },
+      { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+      { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+      { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
+    },
+    opts = {},
+  },
+  {
+    "Saecki/crates.nvim",
+    enabled = false,
+  },
 }
---
--- return {
---   {
---     "NeogitOrg/neogit",
---     dependencies = {
---       "nvim-lua/plenary.nvim",  -- required
---       "sindrets/diffview.nvim", -- optional - Diff integration
---
---       -- Only one of these is needed.
---       "nvim-telescope/telescope.nvim", -- optional
---       "ibhagwan/fzf-lua",              -- optional
---       "echasnovski/mini.pick",         -- optional
---     },
---     config = true
---   },
---   {
---   },
---   {
---     "mfussenegger/nvim-dap",
---     enabled = true,
---     init = function()
---       vim.api.nvim_set_keymap('n', '<Leader>db', '<cmd> DapToggleBreakpoint <CR>', {})
---       -- vim.keymap.set('n', '<Leader>dus', function()
---       --   local widgets = require("dap.ui.widgets")
---       --   local sidebar = widgets.sidebar(widgets.scopes)
---       --   sidebar.open()
---       -- end
---       -- , {})
---     end,
---   },
---   {
---     "leoluz/nvim-dap-go",
---     ft = "go",
---     enabled = true,
---     dependencies = {
---       "mfussenegger/nvim-dap",
---     },
---     config = function(_, opts)
---       require("dap-go").setup(opts)
---     end,
---     init = function()
---       vim.keymap.set('n', '<Leader>dgt', function()
---         require("dap-go").debug_test()
---       end
---       , {})
---       vim.keymap.set('n', '<Leader>dgl', function()
---         require("dap-go").debug_last()
---       end
---       , {})
---     end,
---   },
---   {
---     "olexsmir/gopher.nvim",
---     ft = "go",
---     build = function()
---       if not require("lazy.core.config").spec.plugins["mason.nvim"] then
---         vim.print "Installing go dependencies..."
---         vim.cmd.GoInstallDeps()
---       end
---     end,
---     dependencies = {
---       "nvim-lua/plenary.nvim",
---       "nvim-treesitter/nvim-treesitter",
---       { "williamboman/mason.nvim", optional = true }, -- by default use Mason for go dependencies
---     },
---     opts = {},
---     mappings = {
---
---     }
---   },
---  -- {
---  -- },
---   -- {
---   --   "williamboman/mason.nvim",
---   --   opts = function(_, opts)
---   --     vim.list_extend(opts.ensure_installed, {
---   --       "gleam",
---   --     })
---   --   end,
---   -- },
---   -- {
---   --   "github/copilot.vim",
---   --   lazy = false,
---   -- },
--- }
---

c3c5d5acb73682762f28c51b122d99c46ffc46dd
Henry G <henry@chanity.io>
2026-01-10 22:22:49 +0300

    add ai

---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,13 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  lazy = false,
+  enabled = true,
+  config = function()
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<C-j>",
+        accept_word = "<C-k>",
+      },
+    })
+  end,
+}

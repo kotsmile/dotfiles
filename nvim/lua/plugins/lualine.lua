324fa2168105fe46635f69bde617522290a7ddaa
Greg <kotsmilewtm@gmail.com>
2026-02-03 17:13:21 +0300

    add full path for nvim

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -44,7 +44,11 @@ return {
             },
           },
           { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          { LazyVim.lualine.pretty_path() },
+          {
+            function()
+              return vim.fn.fnamemodify(vim.fn.expand("%:p"), ":~")
+            end,
+          },
         },
         lualine_x = {
           Snacks.profiler.status(),

a8fcce350d55598f699122d810fc310eb9c3b0c6
Greg <kotsmilewtm@gmail.com>
2026-02-12 12:39:16 +0300

    patch no-go

--- nvim/lua/plugins/no-go.lua
+++ nvim/lua/plugins/no-go.lua
@@ -6,4 +6,19 @@ return {
   opts = {
     identifiers = { "err", "error" },
   },
+  config = function(_, opts)
+    -- Patch queries to work with latest tree-sitter-go (no more statement_list node)
+    local queries = require("no-go.queries")
+    queries.error_query = [[
+(
+  (if_statement
+    condition: (binary_expression
+      left: (identifier) @err_identifier)
+    consequence: (block
+        (return_statement
+          (expression_list
+            (identifier) @return_identifier)?))) @collapse_block) @if_statement
+]]
+    require("no-go").setup(opts)
+  end,
 }
